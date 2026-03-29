65fda6eb5ec6f11a8c35159b9a56c78e6bc53278
Umang Srivastava <btech10271.23@bitmesra.ac.in_umangUS002>
2026-03-29 18:58:40 +0530

    checked scraper and updated indian company list

--- server/scheduler/jobScheduler.js
+++ server/scheduler/jobScheduler.js
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { addCompanyToQueue } from "../producers/jobProducer.js";
 
-const companies = ["airbnb", "stripe", "notion", "datadog", "coinbase"];
+const companies = ["zomato", "myntra", "swiggy", "slice", "jupiter", "cred", "coinswitchkuber", "browserstack", "chargebee", "freshworks", "licious", "dunzo", "shiprocket", "gleanwork", "observeai", "whatfix", "agoda", "atlassian", "acko", "urbancompany", "postman", "razorpay", "phonepe", "groww", "inmobi", "tekion", "zscaler", "atlan", "hasura", "sigmoid", "alphasense", "airbnb", "stripe", "notion", "datadog", "coinbase", "databricks", "cloudfare"];
 
 cron.schedule("0 2 * * *", async () => {
 
